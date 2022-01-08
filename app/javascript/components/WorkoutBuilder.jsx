659d3bb71743c0adaa757b43fcaa517f14c17b99
Sean Nemann <seannemann@Seans-MacBook-Air.local>
2022-01-08 18:18:38 -0500

    Style

--- app/javascript/components/WorkoutBuilder.jsx
+++ app/javascript/components/WorkoutBuilder.jsx
@@ -41,6 +41,22 @@ const COMPLETE_WORKOUT = gql`
   }
 `;
 
+const CompletedExcercise = ({ workoutExercise }) => (
+  <div>
+    <div className="bg-gray-500 w-56 rounded-md">
+      <div className="text-white ml-4">{workoutExercise.exercise.name}</div>
+    </div>
+    <div>
+      {workoutExercise.strengthSets.map((set) => (
+        <div className="flex ml-4 border-solid border-2 w-48">
+          <div className="w-8 border-l-solid border-r-2">{set.reps}</div>
+          <div>{set.lbs}</div>
+        </div>
+      ))}
+    </div>
+  </div>
+);
+
 const ExerciseCard = ({ title }) => (
   <div className="flex-col w-56">
     <div className="h-20 bg-gray-500 hover:bg-gray-600 flex justify-center items-center">
@@ -94,21 +110,15 @@ const WorkoutBuilder = () => {
 
   return (
     <div>
-      <div>{workout.startedAt}</div>
+      <div className="ml-4 text-lg mb-4">
+        Start Time: {new Date(workout.startedAt).toLocaleTimeString()}
+      </div>
       {workout.workoutExercises.map((workoutExercise) => (
-        <div>
-          <div>{workoutExercise.exercise.name}</div>
-          <div>
-            {workoutExercise.strengthSets.map((set) => (
-              <div>
-                <div>{set.reps}</div>
-                <div>{set.lbs}</div>
-              </div>
-            ))}
-          </div>
+        <div className="ml-4  mb-4 text-lg">
+          <CompletedExcercise workoutExercise={workoutExercise} />
         </div>
       ))}
-      <div className="flex border-b-2 ml-4 mr-4 justify-between w-9/12">
+      <div className="flex border-b-2 ml-4 mr-4 justify-between w-11/12 lg:w-9/12">
         <div className="text-3xl">Add Exercise</div>
         <button
           onClick={completeWorkout}
